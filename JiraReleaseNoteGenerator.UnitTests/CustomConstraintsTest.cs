361b9ddd102a458197aad3138a354d8d3181c2f1
Patrick Größ <patrick.groess@rubicon.eu>
2010-01-12 13:51:53 +0000

    Added CustomConstraints object which includes version and addition jql query

---
+++ JiraReleaseNoteGenerator.UnitTests/CustomConstraintsTest.cs
@@ -0,0 +1,44 @@
+// Copyright (C) 2005 - 2009 rubicon informationstechnologie gmbh
+// All rights reserved.
+//
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Remotion.BuildTools.JiraReleaseNoteGenerator.UnitTests
+{
+  [TestFixture]
+  public class CustomConstraintsTest
+  {
+    [Test]
+    public void CustomConstraints_additionalConstraintIsNull ()
+    {
+      var customConstraints = new CustomConstraints ("1.2", null);
+
+      Assert.That (customConstraints.Version, Is.EqualTo ("1.2"));
+      Assert.That (customConstraints.AdditionalConstraint, Is.Null);
+    }
+
+    [Test]
+    public void CustomConstraints_additionalConstraintNotNull ()
+    {
+      var customConstraints = new CustomConstraints ("1.2", "abc");
+
+      Assert.That (customConstraints.Version, Is.EqualTo ("1.2"));
+      Assert.That (customConstraints.AdditionalConstraint, Is.EqualTo ("abc"));
+    }
+
+    [Test]
+    public void CustomConstraints_Fail ()
+    {
+      try
+      {
+        new CustomConstraints (null, "abc");
+        Assert.Fail();
+      }
+      catch (ArgumentNullException exception)
+      {
+      }
+    }
+  }
+}
