6ca0fe26f7ced4547407211f365668f5c3810b4f
Daniel Sklenitzka <daniel.sklenitzka@rubicon.eu>
2011-09-23 09:09:17 +0000

    NuGet MsBuild-Task

---
+++ MSBuildTasks/NuGet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace Remotion.BuildTools.MSBuildTasks
+{
+  public class NuGet : ToolTask
+  {
+    [Required]
+    public ITaskItem NuSpecFile { get; set; }
+    
+    [Required]
+    public ITaskItem NuGetPath { get; set; }
+
+    protected override string GenerateFullPathToTool ()
+    {
+      return NuGetPath.ItemSpec;
+    }
+
+    protected override string GenerateCommandLineCommands ()
+    {
+      return String.Format("pack {0} -sym", NuSpecFile.ItemSpec);
+    }
+
+    protected override string ToolName
+    {
+      get { return "NuGet"; }
+    }
+  }
+}
