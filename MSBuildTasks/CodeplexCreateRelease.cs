c4a3aebca978329787d00e7ea100edd37d4a94f9
Dominik Rauch <dominik.rauch@rubicon.eu>
2012-12-18 09:26:24 +0000

    Added CodePlex tasks (RM-5053)

---
+++ MSBuildTasks/CodeplexCreateRelease.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Remotion.BuildTools.MSBuildTasks
+{
+  public class CodeplexCreateRelease : ToolTask
+  {
+    [Required]
+    public string ProjectName { get; set; }
+    [Required]
+    public string ReleaseName { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public bool IsDefaultRelease { get; set; }
+    // ReleaseDate: yyyy-MM-dd
+    [Required]
+    public string ReleaseDate { get; set; }
+    // ReleaseStatus: Planning, Alpha, Beta or Stable
+    [Required]
+    public string ReleaseStatus { get; set; }
+    [Required]
+    public bool ShowToPublic { get; set; }
+
+    protected override string GenerateFullPathToTool ()
+    {
+      return ToolPath;
+    }
+
+    protected override string GenerateCommandLineCommands ()
+    {
+      var showToPublic = ShowToPublic ? '+' : '-';
+      var isDefaultRelease = IsDefaultRelease ? '+' : '-';
+      return string.Format("createRelease /projectName:{0} /releaseName:{1} /description:\"{2}\" /releaseDate:{3} /status:{4} /showToPublic:{5} /isDefaultRelease:{6} /username:{7} /password:{8}", ProjectName, ReleaseName, Description, ReleaseDate, ReleaseStatus, showToPublic, isDefaultRelease, Username, Password);
+    }
+
+    protected override string ToolName
+    {
+      get { return "CodeplexReleaseTool.exe"; }
+    }
+  }
+}
---
+++ MSBuildTasks/CodeplexUploadFiles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Remotion.BuildTools.MSBuildTasks
+{
+  public class CodeplexUploadFiles : ToolTask
+  {
+    [Required]
+    public string ProjectName { get; set; }
+    [Required]
+    public string ReleaseName { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string FileDisplayName { get; set; }
+    [Required]
+    public ITaskItem File { get; set; }
+    // FileType: RuntimeBinary, SourceCode, Documentation or Example
+    [Required]
+    public string FileType { get; set; }
+
+    protected override string GenerateFullPathToTool ()
+    {
+      return ToolPath;
+    }
+
+    protected override string GenerateCommandLineCommands ()
+    {
+      var fileDataPath = File.ToString();
+      return string.Format("uploadReleaseFiles /projectName:{0} /releaseName:{1} /fileDisplayName:{2} /fileDataPath:{3} /fileType:{4} /username:{5} /password:{6}", ProjectName, ReleaseName, FileDisplayName, fileDataPath, FileType, Username, Password);
+    }
+
+    protected override string ToolName
+    {
+      get { return "CodeplexReleaseTool.exe"; }
+    }
+  }
+}
