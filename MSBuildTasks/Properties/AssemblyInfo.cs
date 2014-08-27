75303f762e559498079123d27632e2f7fa0c2df5
Michael Ketting <michael.ketting@rubicon.eu>
2014-08-27 13:02:43 +0000

    Added nuget package for MSBuildTasks.

--- MSBuildTasks/Properties/AssemblyInfo.cs
+++ MSBuildTasks/Properties/AssemblyInfo.cs
@@ -15,36 +15,13 @@
 // under the License.
 // 
 using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle ("Remotion MSBuildTasks")]
+[assembly: AssemblyTitle ("re-motion MSBuild Tasks")]
 [assembly: AssemblyDescription ("Various MSBuild tasks used by the re-motion build.")]
-[assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyProduct ("Remotion MSBuildTasks")]
 [assembly: AssemblyCompany ("rubicon IT GmbH")]
 [assembly: AssemblyCopyright ("Copyright (c) rubicon IT GmbH, www.rubicon.eu - Licensed under Apache License, Version 2.0")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("955f3656-f429-4944-91af-b50be9b2a142")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyVersion ("1.0.*")]
